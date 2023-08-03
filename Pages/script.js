bb228a080d4a56ffe6fa94517dea8e6af649ae1b
Kashif <ahmed.kashif1001@gmail.com>
2023-08-03 20:35:29 +0500

    first-commit

---
+++ Pages/script.js
@@ -0,0 +1,59 @@
+//creating signin and signup variables
+let signUpButton = document.getElementById('signUp');
+let signInButton = document.getElementById('signIn');
+let container = document.getElementById('container');
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+//creating function to signup
+function signup(event){
+  event.preventDefault();
+
+  let name_variable = document.getElementById("signup_name").value
+  let email_variable = document.getElementById("signup_email").value
+  let password_variable = document.getElementById("signup_password").value
+  console.log(name_variable, " ", email_variable, " ", password_variable);
+
+  localStorage.setItem("names", name_variable)
+  localStorage.setItem("emails", email_variable)
+  localStorage.setItem("passwords", password_variable)
+}
+
+//function to reset
+function resetFields(){
+  document.getElementById("name").reset()
+  document.getElementById("email").reset()
+  document.getElementById("password").reset()
+}
+
+//creating function to login
+function login(event){
+  event.preventDefault();
+
+  let email_variable=document.getElementById("login_email").value
+  let password_variable=document.getElementById("login_password").value
+
+//setting local storage for emails and passwords
+  let emails=localStorage.getItem("emails")
+  let passwords=localStorage.getItem("passwords")
+
+  console.log(email_variable, "--->", emails);
+  console.log(password_variable, "--->", passwords);
+
+//conditons for successful and unsuccessful logins
+
+  if (email_variable==emails && password_variable==passwords) {
+    console.log("login successful");
+    window.location.href="../Pages/todo/index.html"
+  } else {
+    console.log("Incorrect email and password");
+    alert("Re-enter credentials or create new account")
+  }
+}
+//end//
---
+++ Pages/todo/script.js
@@ -0,0 +1,46 @@
+const inputBox=document.getElementById('input-box')
+const listContainer=document.getElementById('list-container')
+
+//creating a function to add tasks
+function addTask(){
+    if (inputBox.value === '') {
+        alert("Please write something") //alert to let user enter something
+    } else {
+        //following code enters an element to the list
+        let li=document.createElement("li")
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li) //list will be made under the appendChild function
+
+        //making cross icon to delete the list elements
+        let span=document.createElement("span")
+        span.innerHTML="\u00d7"
+        li.appendChild(span)
+    }
+
+    inputBox.value=''
+    saveData()
+}
+
+//creating an event listener to store the tasks
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName==='LI') {
+        e.target.classList.toggle("checked")
+        saveData()
+    } else if (e.target.tagName==="SPAN") {
+        e.target.parentElement.remove()
+        saveData()
+    }
+}, false)
+
+//creating a function to save data
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+//creating a function to show elements
+function showData(){
+    listContainer.innerHTML=localStorage.getItem("data")
+}//calling function to show the data
+showData()
+
+//end//
